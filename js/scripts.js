4bba73049b84eeb56809f8bde9cde867e19eb494
Andrew Parks and Michael Woldemedihin <michael@>
2017-07-11 15:41:25 -0700

    Create basic form with chekbox input and collect user input using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$("form").submit(function(event){
+event.preventDefault();
+$("work-responses").show();
+$("input:checkbox[name=work-transportation]:checked").each(function(){
+    var workTransportationMode = $(this).val();
+    $('#work-responses').append(workTransportationMode + "<br>");
+        });
+        $('#transportation_survey').hide();
+
+});
+  });
